b6f926c583c39b0a46187369ac62deab26a8696a
Margaret Beldyk <margaret@fitocracy.com>
2013-10-18 17:12:59 -0700

    backend for thingy that still does nothing

--- src/com/example/cupcake/TimerActivity.java
+++ src/com/example/cupcake/TimerActivity.java
@@ -2,14 +2,44 @@ package com.example.cupcake;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class TimerActivity extends Activity {
-
+public class TimerActivity extends Activity implements OnClickListener {
+	
+	private static final String TAG = "TimerActivity";
+	
+	private ImageButton upArrow;
+	private ImageButton downArrow;
+	private TextView minutesText; 
+	private TextView secondsText;
+	private Button startButton;
+	
+	private int minutes;
+	private int seconds;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timer);
+		
+		upArrow = (ImageButton) findViewById(R.id.upArrow);
+		downArrow = (ImageButton) findViewById(R.id.downArrow);
+		minutesText = (TextView) findViewById(R.id.minute);
+		secondsText = (TextView) findViewById(R.id.second);
+		startButton = (Button) findViewById(R.id.startbutton);
+		
+		minutes = Integer.parseInt(minutesText.getText().toString());
+		seconds = Integer.parseInt(secondsText.getText().toString());
+		
+		upArrow.setOnClickListener(this);
+		downArrow.setOnClickListener(this);
+		startButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +47,25 @@ public class TimerActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.timer, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		switch(v.getId()) {
+		case R.id.upArrow:
+			Log.d(TAG, "Pressed the up arrow");
+			break;
+		case R.id.downArrow:
+			Log.d(TAG, "Pressed the down arrow");
+			break;
+		case R.id.startbutton:
+			Log.d(TAG, "Pressed the start button");
+			break;
+		default:
+			Log.wtf(TAG, "That's not even a button");
+		}
+		
 	}
 
 }
