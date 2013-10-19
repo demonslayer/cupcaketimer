2584317b18c0deace58668e114b6a36c361d01f4
Margaret Beldyk <margaret@fitocracy.com>
2013-10-18 18:16:23 -0700

    does timery things

--- src/com/example/cupcake/TimerActivity.java
+++ src/com/example/cupcake/TimerActivity.java
@@ -1,6 +1,7 @@
 package com.example.cupcake;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
@@ -23,6 +24,10 @@ public class TimerActivity extends Activity implements OnClickListener {
 	private int minutes;
 	private int seconds;
 	
+	private int lastSetMinutes = 25;
+	
+	private CountDownTimer timer;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,13 +38,15 @@ public class TimerActivity extends Activity implements OnClickListener {
 		minutesText = (TextView) findViewById(R.id.minute);
 		secondsText = (TextView) findViewById(R.id.second);
 		startButton = (Button) findViewById(R.id.startbutton);
-		
-		minutes = Integer.parseInt(minutesText.getText().toString());
-		seconds = Integer.parseInt(secondsText.getText().toString());
+				
+		updateText();
 		
 		upArrow.setOnClickListener(this);
 		downArrow.setOnClickListener(this);
 		startButton.setOnClickListener(this);
+				
+		makeTimer();
+		
 	}
 
 	@Override
@@ -55,17 +62,57 @@ public class TimerActivity extends Activity implements OnClickListener {
 		switch(v.getId()) {
 		case R.id.upArrow:
 			Log.d(TAG, "Pressed the up arrow");
+			minutes++;
 			break;
 		case R.id.downArrow:
 			Log.d(TAG, "Pressed the down arrow");
+			if (minutes > 0) {
+				minutes--;
+			}
 			break;
 		case R.id.startbutton:
 			Log.d(TAG, "Pressed the start button");
+			timer.start();
 			break;
 		default:
 			Log.wtf(TAG, "That's not even a button");
 		}
 		
+		makeTimer();
+		updateText();
+		lastSetMinutes = minutes;
+		
+	}
+	
+	private void updateText() {
+		secondsText.setText(String.format("%02d", seconds));
+		minutesText.setText(String.format("%02d",minutes));
+	}
+
+	private void makeTimer() {
+		this.timer = new CountDownTimer(minutes*60*1000, 1000) {
+
+			@Override
+			public void onFinish() {
+				Log.d(TAG, "Timer done");
+				seconds = 0;
+				minutes = lastSetMinutes;
+				
+				updateText();
+			}
+
+			@Override
+			public void onTick(long millisUntilFinished) {
+				if (seconds <= 0) {
+					seconds = 59;
+					minutes--;
+				} else {
+					seconds--;
+				}
+				
+				updateText();
+			}
+		};
 	}
 
 }
